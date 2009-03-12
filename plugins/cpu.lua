f025c104d62e170734d90273bfc1ef4cb626a343
dcurtis <dcurtis@cs.uiowa.edu>
2009-03-11 22:16:11 -0500

    changes.

--- plugins/cpu.lua
+++ plugins/cpu.lua
@@ -1,133 +1,95 @@
---[[
-=pod
-
-=head1 NAME
-
-cpu.lua - wmiirc-lua plugin for monitoring acpi stuff
-
-=head1 SYNOPSIS
-
-    -- in your wmiirc.lua:
-    wmii.load_plugin("cpu")
-
-
-=head1 DESCRIPTION
-
-=head1 SEE ALSO
-
-L<wmii(1)>, L<lua(1)>
-
-=head1 AUTHOR
-
-Jan-David Quesel <jdq@gmx.net>
-
-=head1 LICENCE AND COPYRIGHT
-
-Copyright (c) 2008, Jan-David Quesel <jdq@gmx.net>
-
-This is free software.  You may redistribute copies of it under the terms of
-the GNU General Public License L<http://www.gnu.org/licenses/gpl.html>.  There
-is NO WARRANTY, to the extent permitted by law.
-
-=cut
---]]
-
+--
+-- Copyright (c) 2008, Dave O'Neill <dmo@dmo.ca>
+--
+-- Simple load applet for wmii bar.
+--
 local wmii = require("wmii")
-local os = require("os")
-local posix = require("posix")
 local io = require("io")
+local math = require("math")
+local string = require("string")
+local table = require("table")
+local ipairs = ipairs
+local tonumber = tonumber
 local type = type
-local error = error
-local pairs = pairs
-local tostring = tostring
 
 module("cpu")
-api_version = 0.1
+api_version=0.1
 
--- ------------------------------------------------------------
--- MODULE VARIABLES
-local widget = nil
-local timer  = nil
+local palette = { 
+    "#888888",
+    "#999988",
+    "#AAAA88",
+    "#BBBB88",
 
-widget = wmii.widget:new ("400_cpu")
+    "#CCCC88",
+    "#CCBB88",
+    "#CCAA88",
 
-local function cpu_list()
-        local dir = "/sys/devices/system/cpu/"
-        local _,cpu
-        local list = {}
-        for _,cpu in pairs(posix.glob(dir .. 'cpu[0-9]*')) do
-                local stat
-                if cpu then
-                        stat = posix.stat(cpu)
-                        if stat and stat.type == 'directory' then
-                                list[#list+1] = cpu
-                        end
-                end
+    "#DD9988",
+    "#EE8888",
+    "#FF4444",
+}
+
+local widgets = {}
+
+local cpu = {}
+
+local function updatewidget(i, current_avg)
+    local colors
+    if type(current_avg) == "number" then
+        local index  = math.min(math.floor((current_avg/100) * (#palette-1)) + 1, #palette)
+        local bg = wmii.get_conf("loadavg.bg")
+        if bg then
+            colors = table.concat({palette[index], bg, bg}, ' ')
+        else
+            local normal = wmii.get_ctl("normcolors")
+            colors = string.gsub(normal, "^%S+", palette[index], 1)
         end
-        return list
+    end
+    widgets[i]:show(current_avg .."%", colors)
 end
 
+local function loadavg_timer (time_since_update)
+    local file = io.open('/proc/stat')
 
-function read_file(path)
-      local fd = io.open(path, "r")
-      if fd == nil then
-              return nil
-      end
+    local info = {}
+    for line in file:lines() do
+        if line:match("^cpu%d") then
+            local fields = {}
+            sep = " *"
+            line:gsub("([^"..sep.."]*)"..sep, function(c) table.insert(fields, c) end)
+            table.insert(info, fields)
+        end
+    end
+    file:close()
 
-      local text = fd:read("*a")
-       fd:close()
+    while #cpu < #info do
+        table.insert(cpu, {usage = 0, total = 0, active = 0})
+        table.insert(widgets, wmii.widget:new ("400_cpu"..(#cpu+1)))
+    end
 
-       if type(text) == 'string' then
-               text = text:match('(%w+)')
-       end
+    local text = ""
+    for i,v in ipairs(info) do
+        local total = v[2] + v[3] + v[4] + v[5]
+        local active = v[2] + v[3] + v[4]
 
-      return text
+        local diff_total = total - cpu[i].total
+        local diff_active = active - cpu[i].active
+
+        cpu[i].usage = math.floor(diff_active / diff_total *100)
+
+        cpu[i].total = total
+        cpu[i].active = active
+
+        updatewidget(i, cpu[i].usage)
+    end
+
+    -- Returns:
+    -- 	positive number of seconds before next wakeup
+    -- 	nil, or no return, to repeat the last schedule
+    -- 	-1 to stop the timer
+    return 5
 end
 
+local timer = wmii.timer:new( loadavg_timer, 1)
 
-local function create_string(cpu)
-       local govfile = cpu .. '/cpufreq/scaling_governor'
-       local gov = read_file(govfile) or ""
-
-       local frqfile = cpu .. '/cpufreq/scaling_cur_freq'
-       local frq = read_file(frqfile) or ""
-
-       if type(frq) == 'string' then
-               local mhz = frq:match('(.*)000')
-               if mhz then
-                       frq = mhz .. " MHz"
-               else
-                       frq = frq .. "kHz"
-               end
-       else
-               frq = ""
-       end
-
-      return frq .. "(" .. gov .. ")" 
-end
-
-
-function update ( new_vol )
-       local txt = ""
-       local _, cpu
-       local list = cpu_list()
-	   local space = ""
-       for _,cpu in pairs(list) do
-		   local str = create_string(cpu)
-		   if txt == str then
-			   txt = "2x " .. txt
-		   else
-               txt = txt .. create_string(cpu) .. space
-		   end
-		   space = " "
-       end
-
-	widget:show(txt)
-end
-
-local function cpu_timer ( timer )
-	update(0)
-    return 10
-end
-
-timer = wmii.timer:new (cpu_timer, 1)
